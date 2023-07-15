af41f446dc8c6ab8bd2a5d4bd12edd5ee45ef1d4
Paulina-Maciesowicz <paulinamaa@gmail.com>
2023-07-15 13:01:52 +0200

    update
    update

---
+++ src/components/Trending/GetTrendingItem/GetTrendingItem.jsx
@@ -0,0 +1,18 @@
+import PropTypes from 'prop-types';
+import css from './GetTrendingItem.module.css';
+
+export const MovieGalleryItem = ({ movie, setSelectedMovie }) => (
+  <li className={css.movieGalleryItem}>
+    <img
+      className={css.movieGalleryItemMovie}
+      src={movie.webformatURL}
+      alt={movie.id}
+      onClick={() => setSelectedMovie(movie.largeImageURL)}
+    />
+  </li>
+);
+
+MovieGalleryItem.propTypes = {
+  // image: PropTypes.object.isRequired,
+  // setSelectedImage: PropTypes.func.isRequired,
+};
