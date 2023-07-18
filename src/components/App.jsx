9e4b91fbe73df8590661044ab8121cef3945e440
Paulina-Maciesowicz <paulinamaa@gmail.com>
2023-07-18 12:33:40 +0200

    update
    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import { Home } from '../pages/Home';
 import { Movies } from '../pages/Movies';
 import { MovieDetails } from '../pages/MovieDetails';
-import { NotFound } from '../pages/NotFound';
+// import { NotFound } from '../pages/NotFound';
 import { Container, Header, Logo, Link } from './App.styled';
 
 export const App = () => {
@@ -28,7 +28,7 @@ export const App = () => {
         <Route path="/movies" element={<Movies />} />
         <Route path="/movies/:movieId" element={<MovieDetails />} />
 
-        <Route path="*" element={<NotFound />} />
+        <Route path="*" element={<Home />} />
       </Routes>
     </Container>
   );
